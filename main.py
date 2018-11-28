2e27cef6a1a3745044e94b566e7b1f06a1a4c01c
Thabang Mabula <txb.mabula@gmail.com>
2018-11-28 16:25:36 +0200

    GridCoordinate: limited variable scope

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 class GridCoordinate:
     """Class that models a two-dimensional coordinate"""
 
-    __x: int = 0
-    __y: int = 0
-
     def __init__(self, xpos, ypos):
         """
         Constructor for Coordinate Class
@@ -39,6 +36,5 @@ class GridCoordinate:
 
     def __repr__(self):
         return "Coordinate(%d, %d)" % (self.x, self.y)
-
 
 
