6fac30cba66c1a3c972e63c83f9c3269bc7b0ff8
Thabang Mabula <txb.mabula@gmail.com>
2018-11-29 10:38:44 +0200

    Board: Class that models the gameplay board. Compile Error: class attributes not being noticed by class methods

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class GridCoordinate:
     """Class that models a two-dimensional coordinate"""
 
@@ -57,7 +60,7 @@ class Cell:
 
     def __init__(self):
         """Default constructor"""
-        self.__val = Cell.Mark.NOUGHT
+        self.__val: int = Cell.Mark.NOUGHT
 
     def __str__(self):
         if self.__val == Cell.Mark.NOUGHT:
@@ -88,6 +91,62 @@ class Cell:
         return int(self.__val)
 
 
-test = Cell()
-test.markWithCross()
-print(test)
+class Board:
+    """Class that models a 3X3 playing board"""
+    __SIZE_OF_BOARD = 3
+
+    def __int__(self):
+        self.__positionsFilled: int = 0
+        # Generate a list of cells to generate a board
+        self.__board: List[List[Cell]] = []
+        """
+        for row in range(0, Board.__SIZE_OF_BOARD):
+            rowList: List[Cell] = []
+            for column in range(0, Board.__SIZE_OF_BOARD):
+                newCell = Cell()
+                rowList.append(newCell)
+            self.__board.append(rowList)
+        """
+
+    def markWithCross(self, position: GridCoordinate) -> None:
+        """
+        Marks a specified cell in the board with a cross
+
+        param: GridCoordinate position: The coordinate of the cell to be marked
+        rtype: None
+        """
+        if (self.__board[position.x()][position.y()].value() == Cell.Mark.EMPTY):
+            self.__board[position.x][position.y].markWithCross()
+            self.__positionsFilled += 1
+
+    def markWithNought(self, position: GridCoordinate) -> None:
+        """
+        Marks a specified cell in the board with a nought
+
+        param: GridCoordinate position: The coordinate of the cell to be marked
+        rtype: None
+        """
+        if (self.__board[position.x()][position.y()].value() == Cell.Mark.EMPTY):
+            self.__board[position.x][position.y].markWithNought()
+            self.__positionsFilled +=1
+
+    def value(self, position: GridCoordinate) -> int:
+        """
+        Returns the numeric value of a specified cell in the board
+
+         param: GridCoordinate position: The coordinate of the cell to be marked
+         rtype: int
+         """
+        return int(self.__board[position.x()][position.y()].value())
+
+    def isFull(self) -> bool:
+        """
+        Returns boolean indicating whether the board is full or not
+
+        rtype: bool
+        """
+        return bool(self.__positionsFilled == pow(Board.__SIZE_OF_BOARD, 2))
+
+
+test = Board()
+print(test.isFull())
