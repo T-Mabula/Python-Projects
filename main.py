6901f3527b0a3cb93b0ca23402bcbc26b0396e60
Thabang Mabula <txb.mabula@gmail.com>
2018-11-29 07:12:28 +0200

    Cell: A class that models a cell in a grid

--- main.py
+++ main.py
@@ -38,3 +38,41 @@ class GridCoordinate:
         return "Coordinate(%d, %d)" % (self.x, self.y)
 
 
+class Cell:
+    """Class that models the a cell in  grid"""
+
+    __EMPTY_CELL_VAL = 0
+    __CROSS_VAL = 1
+    __NOUGHT_VAL = -1
+
+    def __init__(self):
+        """Default constructor"""
+        self.__val = self.__EMPTY_CELL_VAL
+
+    def __str__(self):
+        if self.__val == self.__NOUGHT_VAL:
+            return 'O'
+        elif self.__val == self.__CROSS_VAL:
+            return 'X'
+        else:
+            return ' '
+
+    def __repr__(self):
+        return "Cell()"
+
+    def markWithCross(self):
+        """Marks the cell with a cross"""
+        self.__val = self.__CROSS_VAL
+
+    def markWithNought(self):
+        """Marks the cell with a nought"""
+        self.__val = self.__NOUGHT_VAL
+
+    @property
+    def value(self):
+        """Returns the numeric value of the cell
+
+        return: Numeric value of the mark in the cell
+        rtype: int
+        """
+        return int(self.__val)
