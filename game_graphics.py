75f6e32aefd0e55f98368c7f45a7928e57543b9b
Thabang Mabula <txb.mabula@gmail.com>
2018-12-10 13:14:47 +0200

    Game graphics: grid marking positions

--- game_graphics.py
+++ game_graphics.py
@@ -6,16 +6,40 @@ class MyFirstGUI:
         master.title("Tic-Tac-Toe")
 
         self.label = Label(master, text="Tic-Tac-Toe")
-        self.label.pack()
+        self.label.grid(columnspan=2)
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack(side = LEFT)
+        self.NW_button = Button(master, text="(1, 1)", command=self.disp('1, 1'))
+        self.NW_button.grid(row = 1, column = 1)
+
+        self.N_button = Button(master, text="(1, 2)", command=self.disp('1, 2'))
+        self.N_button.grid(row=1, column=2)
+
+        self.NE_button = Button(master, text="(1, 3)", command=self.disp('1, 3'))
+        self.NE_button.grid(row=1, column=3)
+
+        self.W_button = Button(master, text="(2, 1)", command=self.disp('2, 1'))
+        self.W_button.grid(row=2, column=1)
+
+        self.C_button = Button(master, text="(2, 2)", command=self.disp('2, 2'))
+        self.C_button.grid(row=2, column=2)
+
+        self.E_button = Button(master, text="(2, 3)", command=self.disp('2, 3'))
+        self.E_button.grid(row=2, column=3)
+
+        self.NE_button = Button(master, text="(3, 1)", command=self.disp('3, 1'))
+        self.NE_button.grid(row = 3, column = 1)
 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack(side = RIGHT)
+        self.close_button.grid(columnspan = 3, row = 4)
 
-    def greet(self):
-        print("Greetings!")
+        self.S_button = Button(master, text="(3, 2)", command=self.disp('3, 2'))
+        self.S_button.grid(row=3, column=2)
+
+        self.SE_button = Button(master, text="(3, 3)", command=self.disp('3, 3'))
+        self.SE_button.grid(row=3, column=3)
+
+    def disp(self, pos: str):
+        print(pos)
 
 root = Tk()
 my_gui = MyFirstGUI(root)
