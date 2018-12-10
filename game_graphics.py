f5c08c0b2cc3510919e0bcd5e0ac277d493eed62
Thabang Mabula <txb.mabula@gmail.com>
2018-12-10 13:37:29 +0200

    Game display: improvement in button positioning

--- game_graphics.py
+++ game_graphics.py
@@ -1,42 +1,44 @@
 from tkinter import *
 
 class MyFirstGUI:
+    btn_width = 12
+    btn_height = 4
     def __init__(self, master):
         self.master = master
         master.title("Tic-Tac-Toe")
 
         self.label = Label(master, text="Tic-Tac-Toe")
-        self.label.grid(columnspan=2)
+        self.label.grid(columnspan=3)
 
-        self.NW_button = Button(master, text="(1, 1)", command=self.disp('1, 1'))
+        self.NW_button = Button(master, text="NW", height = self.btn_height, width = self.btn_width,  command=self.disp('1, 1'))
         self.NW_button.grid(row = 1, column = 1)
 
-        self.N_button = Button(master, text="(1, 2)", command=self.disp('1, 2'))
+        self.N_button = Button(master, text="N", height = self.btn_height, width = self.btn_width, command=self.disp('1, 2'))
         self.N_button.grid(row=1, column=2)
 
-        self.NE_button = Button(master, text="(1, 3)", command=self.disp('1, 3'))
+        self.NE_button = Button(master, text="NE", height = self.btn_height, width = self.btn_width, command=self.disp('1, 3'))
         self.NE_button.grid(row=1, column=3)
 
-        self.W_button = Button(master, text="(2, 1)", command=self.disp('2, 1'))
+        self.W_button = Button(master, text="W", height = self.btn_height, width = self.btn_width, command=self.disp('2, 1'))
         self.W_button.grid(row=2, column=1)
 
-        self.C_button = Button(master, text="(2, 2)", command=self.disp('2, 2'))
+        self.C_button = Button(master, text="C", height = self.btn_height, width = self.btn_width, command=self.disp('2, 2'))
         self.C_button.grid(row=2, column=2)
 
-        self.E_button = Button(master, text="(2, 3)", command=self.disp('2, 3'))
+        self.E_button = Button(master, text="E", height = self.btn_height, width = self.btn_width, command=self.disp('2, 3'))
         self.E_button.grid(row=2, column=3)
 
-        self.NE_button = Button(master, text="(3, 1)", command=self.disp('3, 1'))
+        self.NE_button = Button(master, text="SW", height = self.btn_height, width = self.btn_width, command=self.disp('3, 1'))
         self.NE_button.grid(row = 3, column = 1)
+
+        self.S_button = Button(master, text="S", height = self.btn_height, width = self.btn_width, command=self.disp('3, 2'))
+        self.S_button.grid(row=3, column=2)
+
+        self.SE_button = Button(master, text="SE",height = self.btn_height, width = self.btn_width, command=self.disp('3, 3'))
+        self.SE_button.grid(row=3, column=3)
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.grid(columnspan = 3, row = 4)
-
-        self.S_button = Button(master, text="(3, 2)", command=self.disp('3, 2'))
-        self.S_button.grid(row=3, column=2)
-
-        self.SE_button = Button(master, text="(3, 3)", command=self.disp('3, 3'))
-        self.SE_button.grid(row=3, column=3)
 
     def disp(self, pos: str):
         print(pos)
